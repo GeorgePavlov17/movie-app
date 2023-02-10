fbc506fcb95c606d6a48fd1c7f0af5335ec474d1
Georgi Pavlov <georgipavlov17@gmail.com>
2023-02-10 12:20:12 +0200

    fix for genres page

--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -10,24 +10,24 @@ export class GenresComponent implements OnInit {
   
   public genresList: any[] = [];
   public genreListArr: any[] = [];
+  public selectedGenre: any;
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
     this.movieService.getMoviesGenre().then((result) => {
       result.genres.forEach((e: any) => {
-        this.genresList.push(e.name);
+        this.genresList.push(e);
       });
     });
     // console.log(this.genresList);
   }
 
-  showMoviesByGenre() {
-    this.genresList.forEach((e: any) => {
-      // console.log(e);
-    });
+  showMoviesByGenre(id: any) {
+    this.selectedGenre = id;
+    this.genreListArr = [];
 
-    this.movieService.getMoviesByGenre('14').then((result) => {
+    this.movieService.getMoviesByGenre(String(id)).then((result) => {
       const currMovieGenre = result.results.map((currGenre: any) => {
         return {
           id: currGenre.id,
